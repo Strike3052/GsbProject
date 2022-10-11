ff151327cae8b03b34d9edad650cceaa63c90deb
jonathan.simon <jonathan.simon@CP-SIO-W10-PG10.bts.sio>
2022-10-11 08:49:13 +0200

    Correction Login Incorrect

--- resources/bdd/gsb_restore.sql
+++ resources/bdd/gsb_restore.sql
@@ -22,6 +22,19 @@ CREATE TABLE IF NOT EXISTS etat (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS comptable (
+  id char(4) NOT NULL,
+  nom char(30) DEFAULT NULL,
+  prenom char(30)  DEFAULT NULL, 
+  login char(20) DEFAULT NULL,
+  mdp char(20) DEFAULT NULL,
+  adresse char(30) DEFAULT NULL,
+  cp char(5) DEFAULT NULL,
+  ville char(30) DEFAULT NULL,
+  dateembauche date DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS visiteur (
   id char(4) NOT NULL,
   nom char(30) DEFAULT NULL,
