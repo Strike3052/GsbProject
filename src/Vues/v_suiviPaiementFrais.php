ac7844954cafa0c3c8b829653753bf63e6a818f2
lyceeayme <lyceeayme@gmail.com>
2022-12-13 18:03:46 +0100

    Ajout style

--- src/Vues/v_suiviPaiementFrais.php
+++ src/Vues/v_suiviPaiementFrais.php
@@ -1,12 +1,37 @@
-<h3>Etat actuel de la demande</h3>
-<?php
-echo "<p>La fiche de frais sélectionnée est <strong>'" . $etatFicheFrais . "'</strong></p>";
-?>
+<div>
+    <h3 class="titregras">Etat actuel de la demande</h3>
+    <?php
+    echo "<p>La fiche de frais sélectionnée est <strong>'" . $etatFicheFrais . "'</strong></p>";
+
+    switch ($etatFicheFrais) {
+        case 'Fiche créée, saisie en cours':
+            $message = 'Valider et mettre en paiement';
+            $nextStep = 'VA';
+            break;
+        case 'Validée et mise en paiement' :
+            $message = 'Rembourser les frais';
+            $nextStep = 'RB';
+            break;
+    }
+    ?>
+    <div class="Btn-modifetat">
+        <label>Changer l'état de la fiche :</label>
+        <button form="lstVisiteurs" class="btn btn-success" type="submit" name="nextStep" value="<?php echo $nextStep ?>">
+            <?php echo $message ?>
+        </button>
+    </div>
+</div>
 
 <div>
-    <h3>Frais Forfaitisés</h3>
+    <h3 class="titregras">Frais Forfaitisés</h3>
 
     <table>
+        <thead>
+            <tr>
+                <th>Forfait</th>
+                <th>Montant total</th>
+            </tr>
+        </thead>
         <?php
         foreach ($lesFraisForfait as $unFrais) {
             ?>
@@ -24,7 +49,7 @@ echo "<p>La fiche de frais sélectionnée est <strong>'" . $etatFicheFrais . "'<
 if (!empty($lesFraisHorsForfait)) {
     ?>
     <div>
-        <h3>Frais Hors Forfait</h3>
+        <h3 class="titregras">Frais Hors Forfait</h3>
         <table>
             <thead>
                 <tr>
@@ -50,21 +75,3 @@ if (!empty($lesFraisHorsForfait)) {
 } else {
     echo "<h3>Aucun frais hors forfait signalé</h3>";
 }
-
-switch ($etatFicheFrais) {
-    case 'Fiche créée, saisie en cours':
-        $message = 'Valider et mettre en paiement';
-        $nextStep = 'VA';
-        break;
-    case 'Validée et mise en paiement' :
-        $message = 'Rembourser les frais';
-        $nextStep = 'RB';
-        break;
-}
-?>
-
-<button form="lstVisiteurs" class="btn btn-success" type="submit" name="nextStep" value="<?php echo $nextStep ?>">
-    <?php echo $message ?>
-</button>
-
-
